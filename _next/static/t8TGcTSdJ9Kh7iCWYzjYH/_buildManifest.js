d15666bb96c88e7dda8ce4b3e4a268461c3db162
ashishkumar160 <ashishkumar160@users.noreply.github.com>
2021-02-15 17:58:36 +0000

    deploy: da177b5825216dcf6bd06c8bf99d81dfda59e638

---
+++ _next/static/t8TGcTSdJ9Kh7iCWYzjYH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-352527f7a15a0c857372.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-aa18a27ceb3e8be53a95.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
